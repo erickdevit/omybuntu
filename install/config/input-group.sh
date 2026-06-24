902a076cca12f3ad31a4522bfc4ccb88e60751d0
erickdevit <erickwornex@gmail.com>
2026-06-23 23:12:56 -0300

    Install swayosd and add user to video group for brightness/volume controls

--- install/config/input-group.sh
+++ install/config/input-group.sh
@@ -1,2 +1,2 @@
-# Give this user privileged input access for dictation tools + xbox controllers to work
-sudo usermod -aG input ${USER}
+# Give this user privileged input and video access for dictation tools, xbox controllers, and brightness controls to work
+sudo usermod -aG input,video ${USER}

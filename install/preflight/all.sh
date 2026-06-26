190710920c456515758111ff66b9b04ce55b4124
erickdevit <erickwornex@gmail.com>
2026-06-26 16:56:43 -0300

    fix(preflight): dearmor mise GPG key to binary format for APT

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -9,8 +9,8 @@ if [[ -n ${OMYBUNTU_ONLINE_INSTALL:-} ]]; then
 
   # Add official mise repository
   sudo install -dm 755 /etc/apt/keyrings
-  curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.pub > /dev/null
-  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.pub arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+  curl -fsSL https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise.gpg > /dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise.gpg arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 
   sudo apt-get update
 fi
